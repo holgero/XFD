7e309965621418de6dd75fe95bd9f29874f073d9
Holger Oehm <holger.oehm@holger-oehm.de>
2012-07-08 14:48:14 +0200

    Fix for windows 64 bit
    and ignore eclipse project files.

--- java/src/main/java/de/holger_oehm/usb/hid/windows/DeviceInterfaceDetailData.java
+++ java/src/main/java/de/holger_oehm/usb/hid/windows/DeviceInterfaceDetailData.java
@@ -1,26 +1,32 @@
 package de.holger_oehm.usb.hid.windows;
 
+import com.sun.jna.Native;
 import com.sun.jna.Structure;
 
 /**
- * Java representation of the SP_DEVICE_INTERFACE_DETAIL_DATA structure. It
- * contains the path for a device interface.
+ * SP_DEVICE_INTERFACE_DETAIL_DATA structure.
  */
 public class DeviceInterfaceDetailData extends Structure {
-	/**
-	 * The size, in bytes, of the fixed portion of the
-	 * SP_DEVICE_INTERFACE_DETAIL_DATA structure.
-	 */
-	public int cbSize;
-	/**
-	 * A NULL-terminated string that contains the device interface path. This
-	 * path can be passed to Win32 functions CreateFile.
-	 */
-	public char[] devicePath;
+    /**
+     * According to the documentation this should be the size in bytes,
+     * including the C terminator of the devicePath. But in practice you have to
+     * set this field to 5 on windows 32 bit platforms and to 8 on windows 64
+     * bit platforms.
+     */
+    public int cbSize;
+    /**
+     * Device interface path, used by the Win32 CreateFile functions.
+     */
+    public char[] devicePath;
 
-	public DeviceInterfaceDetailData(final int pathLength) {
-		setAlignType(Structure.ALIGN_NONE);
-		cbSize = getNativeSize(int.class, this) + getNativeSize(char.class, this);
-		devicePath = new char[pathLength];
-	}
+    public DeviceInterfaceDetailData(final int pathLength) {
+        if (Native.POINTER_SIZE == 8) {
+            // win64 has a different idea of the correct value of cbSize
+            cbSize = 8;
+        } else {
+            // the value on windows 32 bit is 5
+            cbSize = getNativeSize(int.class, this) + getNativeSize(char.class, this);
+        }
+        devicePath = new char[pathLength];
+    }
 }
